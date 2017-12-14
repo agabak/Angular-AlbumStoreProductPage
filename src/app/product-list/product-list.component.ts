584fba9a415222e1773257e42a454e3e2eda099c
Agaba Kabunga <agaba_k@hotmail.com>
2017-12-13 19:26:18 -0600

    fixing Observable call

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -14,6 +14,6 @@ export class ProductListComponent implements OnInit {
   constructor(private _productService: ProductService) {}
 
   ngOnInit() {
-    this._productService.getProducts().subscribe(response => { this.products =  response; });
+    this._productService.getProducts().subscribe(response => this.products =  response );
   }
 }
